63847a3865abff0c25746c8eb9d61fc5503eba41
Leonardo <Leonardo@10.0.0.100>
2023-06-01 23:38:14 -0300

    Adequação, refatoração e esquematização de código;

---
+++ TrabalhoPOO2/src/main/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import guiproj.TelaInicial;
+import java.text.ParseException;
+
+
+/**
+ *
+ * @author professores
+ */
+public class Main {
+    
+    public static void main(String[] args) throws ParseException{
+        new TelaInicial();
+
+    }    
+}
